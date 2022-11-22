f2e10f0d45c78e043c260972baf7892f1be23f88
sortzis <sortzis@mail.nmc.edu>
2022-11-22 16:41:25 -0500

    Add project files.

---
+++ Models/TheList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Models
+{
+    public class TheList
+    {
+        public int ID { get; set; }
+        [Required(ErrorMessage = "Please enter a description of a to do task.")]
+        public string Description { get; set; }
+
+        [Display(Name = "Created Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime CreatedDate { get; set; }
+    }
+}
